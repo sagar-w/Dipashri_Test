ace0d72bffd19d6407d7f46a9932abd0081f04b1
unknown <you@example.com>
2023-10-24 19:23:57 +0530

    Your commit message

---
+++ App.py
@@ -0,0 +1,116 @@
+# print("Hello World🌎!")
+# print("Hello, ", end="")
+# print("World!")
+# name = "πby3"
+# print("Hello: " + name)
+# print("23", "Oct", "2023", sep="-")
+
+# # Taking input
+# name = input("Enter your name: ")
+# print("Hello, " + name)
+
+# # Using the help() function
+# help(print)
+
+# # Data types
+# # Integers
+# x = 5
+# # Floating-point numbers
+# y = 3.14
+# # Strings
+# text = "Python is great"
+# # Boolean
+# is_true = True
+# # Complex
+# c = 2 + 3j
+
+# type()
+# print(type(x))
+
+# # Type casting
+# age_str = "30"
+# age = int(age_str)
+# height_float = 175.5
+# height_int = int(height_float)
+
+# # Range
+# numbers = list(range(1, 6))
+# even_numbers = list(range(2, 11, 2))
+
+# # String operations
+# message = "Python is fun"
+# length = len(message)
+# upper_case = message.upper()
+# lower_case = message.lower()
+# substring = message[0:6]
+# split_message = message.split(" ")
+
+# # Operators
+# # Arithmetic Operators
+# a = 10
+# b = 3
+# addition = a + b
+# subtraction = a - b
+# multiplication = a * b
+# division = a / b
+# division = a // b
+# modulo = a % b
+# exponentiation = a ** b
+
+# # Logical Operators
+# is_greater = a > b
+# is_equal = a == b
+# logical_and = is_greater and is_equal
+# logical_or = is_greater or is_equal
+# logical_not = not is_greater
+
+
+# # Bitwise Operators
+# bitwise_and = a & b
+# bitwise_or = a | b
+# bitwise_xor = a ^ b
+# bitwise_shift_left = a << 2
+# bitwise_shift_right = a >> 1
+
+
+# # Control flow
+# # If Statement
+# if a > b:
+#     print("a is greater than b")
+
+# # If-Else Statement
+# if a > b:
+#     print("a is greater than b")
+# else:
+#     print("a is not greater than b")
+
+# # If-Else-If (If-Else Ladder)
+# if a > b:
+#     print("a is greater than b")
+# elif a == b:
+#     print("a is equal to b")
+# else:
+#     print("a is less than b")
+
+# # Loops
+# # For Loop
+# for i in range(5):
+#     print("Iteration", i)
+
+# # While Loop
+# count = 0
+# while count < 5:
+#     print("Count:", count)
+#     count += 1
+
+# # Functions
+# # Function definition
+
+
+# def greet(name):
+#     return "Hello, " + name
+
+
+# # Function call
+# result = greet("Alice")
+# print(result)
